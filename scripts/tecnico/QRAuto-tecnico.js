b54148681cdd93eaf7270ff62b6176bebf59ebb4
estelucero <este.lucero2212@gmail.com>
2024-10-06 16:25:17 -0300

    Fix vuelve como antes

--- scripts/tecnico/QRAuto-tecnico.js
+++ scripts/tecnico/QRAuto-tecnico.js
@@ -1,54 +1,54 @@
-// function onScanSuccess(qrMessage) {
-//     document.getElementById('result').innerHTML = `
-//         <h2>Patente Leida Con Exito!</h2>
-//         <p>${qrMessage}</p>
-//         `;
-//         // Prints result as a link inside result element
-
-//         html5QrcodeScanner.clear();
-//         // Clears scanning instance
-
-//         document.getElementById('reader').remove();
-// }
-
-// function onScanError(errorMessage) {
-    
-    
-//     console.error(`Error en el escaneo: ${errorMessage}`);
-// }
-
-// var html5QrcodeScanner = new Html5QrcodeScanner(
-//     "reader", { fps: 30,
-//         qrbox: { width: 350, height: 350 }}
-// );
-// html5QrcodeScanner.render(onScanSuccess, onScanError);
-
-const html5QrCode = new Html5Qrcode("reader");
-const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+function onScanSuccess(qrMessage) {
     document.getElementById('result').innerHTML = `
         <h2>Patente Leida Con Exito!</h2>
-        <p>${decodedText}</p>
-    `;
-    // Detiene el escáner
-    html5QrCode.stop().then(() => {
-        document.getElementById('reader').remove();
-    }).catch(err => {
-        console.error('Error al detener el escáner: ', err);
-    });
-};
-const qrCodeErrorCallback = (errorMessage) => {
-    console.error(`Error en el escaneo: ${errorMessage}`);
-};
+        <p>${qrMessage}</p>
+        `;
+        // Prints result as a link inside result element
 
-const config = { fps: 30, qrbox: { width: 250, height: 250 } };
-html5QrCode.start(
-    { facingMode: "environment" }, 
-    config, 
-    qrCodeSuccessCallback,
-    qrCodeErrorCallback
-).catch(err => {
-    console.error(`Error al iniciar el escaneo: ${err}`);
-});
+        html5QrcodeScanner.clear();
+        // Clears scanning instance
+
+        document.getElementById('reader').remove();
+}
+
+function onScanError(errorMessage) {
+    
+    
+    console.error(`Error en el escaneo: ${errorMessage}`);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 30,
+        qrbox: { width: 350, height: 350 }}
+);
+html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+// const html5QrCode = new Html5Qrcode("reader");
+// const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+//     document.getElementById('result').innerHTML = `
+//         <h2>Patente Leida Con Exito!</h2>
+//         <p>${decodedText}</p>
+//     `;
+//     // Detiene el escáner
+//     html5QrCode.stop().then(() => {
+//         document.getElementById('reader').remove();
+//     }).catch(err => {
+//         console.error('Error al detener el escáner: ', err);
+//     });
+// };
+// const qrCodeErrorCallback = (errorMessage) => {
+//     console.error(`Error en el escaneo: ${errorMessage}`);
+// };
+
+// const config = { fps: 30, qrbox: { width: 250, height: 250 } };
+// html5QrCode.start(
+//     { facingMode: "environment" }, 
+//     config, 
+//     qrCodeSuccessCallback,
+//     qrCodeErrorCallback
+// ).catch(err => {
+//     console.error(`Error al iniciar el escaneo: ${err}`);
+// });
 
 
 
