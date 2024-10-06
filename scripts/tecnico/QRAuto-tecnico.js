a05918acd5661ac1e0a9aba360eec94f8d3834e8
estelucero <este.lucero2212@gmail.com>
2024-10-06 12:04:57 -0300

    Fix camara de atras

--- scripts/tecnico/QRAuto-tecnico.js
+++ scripts/tecnico/QRAuto-tecnico.js
@@ -23,39 +23,29 @@
 // );
 // html5QrcodeScanner.render(onScanSuccess, onScanError);
 
-function onScanSuccess(qrMessage) {
+const html5QrCode = new Html5Qrcode("reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     document.getElementById('result').innerHTML = `
         <h2>Patente Leida Con Exito!</h2>
-        <p>${qrMessage}</p>
+        <p>${decodedText}</p>
     `;
-    
-    // Clears scanning instance
-    html5QrcodeScanner.clear();
-    document.getElementById('reader').remove();
-}
-
-function onScanError(errorMessage) {
+    // Detiene el escáner
+    html5QrCode.stop().then(() => {
+        document.getElementById('reader').remove();
+    }).catch(err => {
+        console.error('Error al detener el escáner: ', err);
+    });
+};
+const qrCodeErrorCallback = (errorMessage) => {
     console.error(`Error en el escaneo: ${errorMessage}`);
-}
+};
 
-Html5Qrcode.getCameras().then(devices => {
-    if (devices && devices.length) {
-        // Busca la cámara trasera
-        const rearCamera = devices.find(device => device.label.toLowerCase().includes('back'));
-        
-        // Si se encuentra la cámara trasera, usa su ID, si no, toma la primera cámara disponible
-        const cameraId = rearCamera ? rearCamera.id : devices[0].id;
-
-        // Crear instancia del escáner con la cámara seleccionada
-        var html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", { fps: 30, qrbox: { width: 350, height: 350 } }
-        );
-        
-        // Renderizar el escaneo utilizando la cámara seleccionada
-        html5QrcodeScanner.render(onScanSuccess, onScanError, cameraId);
-    } else {
-        console.error("No se encontraron cámaras en el dispositivo.");
-    }
-}).catch(err => {
-    console.error("Error al obtener las cámaras: ", err);
+const config = { fps: 30, qrbox: { width: 350, height: 350 } };
+html5QrCode.start(
+    { facingMode: { exact: "environment"} }, 
+    config, 
+    qrCodeSuccessCallback,
+    qrCodeErrorCallback
+).catch(err => {
+    console.error(`Error al iniciar el escaneo: ${err}`);
 });
