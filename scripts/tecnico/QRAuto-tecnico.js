8f6d2a49238282f16b6523c9c7d9dad7503a61cc
estelucero <este.lucero2212@gmail.com>
2024-10-06 11:41:59 -0300

    agrego para que sea solo la camara trasera

--- scripts/tecnico/QRAuto-tecnico.js
+++ scripts/tecnico/QRAuto-tecnico.js
@@ -1,24 +1,61 @@
+// function onScanSuccess(qrMessage) {
+//     document.getElementById('result').innerHTML = `
+//         <h2>Patente Leida Con Exito!</h2>
+//         <p>${qrMessage}</p>
+//         `;
+//         // Prints result as a link inside result element
+
+//         html5QrcodeScanner.clear();
+//         // Clears scanning instance
+
+//         document.getElementById('reader').remove();
+// }
+
+// function onScanError(errorMessage) {
+    
+    
+//     console.error(`Error en el escaneo: ${errorMessage}`);
+// }
+
+// var html5QrcodeScanner = new Html5QrcodeScanner(
+//     "reader", { fps: 30,
+//         qrbox: { width: 350, height: 350 }}
+// );
+// html5QrcodeScanner.render(onScanSuccess, onScanError);
+
 function onScanSuccess(qrMessage) {
     document.getElementById('result').innerHTML = `
-        <h2>Success!</h2>
-        <p><a href="${qrMessage}">${qrMessage}</a></p>
-        `;
-        // Prints result as a link inside result element
-
-        html5QrcodeScanner.clear();
-        // Clears scanning instance
-
-        document.getElementById('reader').remove();
+        <h2>Patente Leida Con Exito!</h2>
+        <p>${qrMessage}</p>
+    `;
+    
+    // Clears scanning instance
+    html5QrcodeScanner.clear();
+    document.getElementById('reader').remove();
 }
 
 function onScanError(errorMessage) {
-    
-    
     console.error(`Error en el escaneo: ${errorMessage}`);
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 30,
-        qrbox: { width: 350, height: 350 }}
-);
-html5QrcodeScanner.render(onScanSuccess, onScanError);
+Html5Qrcode.getCameras().then(devices => {
+    if (devices && devices.length) {
+        // Busca la cámara trasera
+        const rearCamera = devices.find(device => device.label.toLowerCase().includes('back'));
+        
+        // Si se encuentra la cámara trasera, usa su ID, si no, toma la primera cámara disponible
+        const cameraId = rearCamera ? rearCamera.id : devices[0].id;
+
+        // Crear instancia del escáner con la cámara seleccionada
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", { fps: 30, qrbox: { width: 350, height: 350 } }
+        );
+        
+        // Renderizar el escaneo utilizando la cámara seleccionada
+        html5QrcodeScanner.render(onScanSuccess, onScanError, cameraId);
+    } else {
+        console.error("No se encontraron cámaras en el dispositivo.");
+    }
+}).catch(err => {
+    console.error("Error al obtener las cámaras: ", err);
+});
