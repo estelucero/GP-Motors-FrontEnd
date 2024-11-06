dfd102fdd217392bcc67d6b9e0c1e2689b42b92f
estelucero <este.lucero2212@gmail.com>
2024-11-06 17:26:37 -0300

    cambio https

--- scripts/gerente/inicio-gerente.js
+++ scripts/gerente/inicio-gerente.js
@@ -4,11 +4,11 @@ console.log(usuario);
 function agregarReporte() {
   let url = "";
   if (usuario === 1) {
-    url = "http://helping-officially-locust.ngrok-free.app/public/dashboard/a3a2e038-1cd9-4c97-bb2b-b549f8d75472"
+    url = "https://helping-officially-locust.ngrok-free.app/public/dashboard/a3a2e038-1cd9-4c97-bb2b-b549f8d75472"
   } else if (usuario === 2) {
-    url = "http://helping-officially-locust.ngrok-free.app/public/dashboard/158e2542-bd5c-4e84-9ce6-eea73c4bc18f"
+    url = "https://helping-officially-locust.ngrok-free.app/public/dashboard/158e2542-bd5c-4e84-9ce6-eea73c4bc18f"
   } else if (usuario === 29) {
-    url = "http://helping-officially-locust.ngrok-free.app/public/dashboard/e6a42a34-4d25-4105-8085-35d228dee385"
+    url = "https://helping-officially-locust.ngrok-free.app/public/dashboard/e6a42a34-4d25-4105-8085-35d228dee385"
   }
 
 
